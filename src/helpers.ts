cdfc3e1f469cdfb3a1eba5d4b7c861d719ef67ef
phothinmg <phothinmg@disroot.org>
2025-08-14 02:32:19 +0630

    first commit

---
+++ src/helpers.ts
@@ -0,0 +1,37 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { existsSync } from "node:fs";
+export const isObject = (input: any) =>
+  typeof input === "object" && !Array.isArray(input) && input !== null;
+export const isPlainObject = (input: any) =>
+  isObject(input) && Object.keys(input).length === 0;
+export const isPlainArray = (input: any) =>
+  Array.isArray(input) && input.length === 0;
+export const wait = (time: number) =>
+  new Promise((resolve) => setTimeout(resolve, time));
+/**
+ * Remove all files in the given directory.
+ * @param dir The directory to clean.
+ */
+export const cleanDir = async (dir: string) => {
+  const files = await fs.readdir(dir);
+  await Promise.all(files.map((file) => fs.unlink(path.join(dir, file))));
+};
+
+/**
+ * Recursively and forcefully removes a directory and all its contents.
+ * @param {string} dirPath - The path to the directory to remove.
+ */
+export async function forceRemoveDir(dirPath: string) {
+  if (!existsSync(dirPath)) return;
+  for (const entry of await fs.readdir(dirPath)) {
+    const fullPath = path.join(dirPath, entry);
+    const stat = await fs.lstat(fullPath);
+    if (stat.isDirectory()) {
+      await forceRemoveDir(fullPath);
+    } else {
+      await fs.unlink(fullPath);
+    }
+  }
+  await fs.rmdir(dirPath);
+}
